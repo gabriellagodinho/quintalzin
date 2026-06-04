c4a9bf4461fdd075ec4e6e6c68bf4d7ca2d7247f
Gabriella Godinho <132962426+gabriellagodinho@users.noreply.github.com>
2026-06-04 17:37:48 -0400

    ajustes ui/ux

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './features/pages/login/login';
+
 import { ListaProdutosComponent } from './features/produtos/pages/lista-produtos/lista-produtos';
+
 import { DetalheProdutoComponent } from './features/produtos/pages/detalhe-produto/detalhe-produto';
+
+import { MainLayoutComponent } from './shared/layout/main-layout/main-layout';
 
 export const routes: Routes = [
 
@@ -11,14 +16,22 @@ export const routes: Routes = [
   },
 
   {
-    path: 'produtos',
-    component: ListaProdutosComponent
-  },
+    path: '',
+    component: MainLayoutComponent,
 
-  {
-  path: 'produto/:id',
-  loadComponent: () => import('./features/produtos/pages/detalhe-produto/detalhe-produto')
-    .then(m => m.DetalheProdutoComponent)
+    children: [
+
+      {
+        path: 'produtos',
+        component: ListaProdutosComponent
+      },
+
+      {
+        path: 'produto/:id',
+        component: DetalheProdutoComponent
+      }
+
+    ]
   }
 
 ];
---
+++ src/app/shared/layout/main-layout/main-layout.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from '../header/header';
+import { FooterComponent } from '../footer/footer';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
+  ],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.css'
+})
+export class MainLayoutComponent {}
